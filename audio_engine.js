697675c251f6a53fda232aa721f8aa8a458bb216
loganbaby <shifo3456@gmail.com>
2023-01-30 20:51:40 +0300

    ref

---
+++ audio_engine.js
@@ -0,0 +1,52 @@
+var waveSounds = [
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/watertap.wav',
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/watertap(1).wav',
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/waterfall.wav',
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/water-flow(1).wav',
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/water-flow(2).wav',
+    'https://loganbaby.github.io/muteitdemo.github.io/assets/audio/flow-water.wav',
+];
+
+var webApiSounds = [];
+window.onload = initWebApiSounds();
+
+async function initWebApiSounds() {
+    for (let i = 0; i < waveSounds.length; ++i) {
+        webApiSounds[i] = new Pizzicato.Sound({ 
+            source: 'file', 
+            options: { path: waveSounds[i], loop: true }
+        }, function(error) {
+            if (!error)
+                console.log('Sound file ' + waveSounds[i] + ' ready to use');
+        });
+    }
+
+    console.log('Init web api sounds');
+}
+
+function mixRandomArray(arr) { arr.sort(() => Math.random() - 0.5) }
+
+function randomInteger(min, max) { return Math.round(min - 0.5 + Math.random() * (max - min + 1)); }
+
+function generateUniqueLoop(timeToRefresh) {
+    mixRandomArray(webApiSounds);
+
+    for (let i = 0; i < webApiSounds.length; ++i) {
+        webApiSounds[i].volume = 1;
+        webApiSounds[i].play(randomInteger(0, 2), randomInteger(0, 6));
+    }
+
+    setTimeout(function () {
+        mixRandomArray(webApiSounds);
+
+        for (let i = 0; i < webApiSounds.length; ++i) {
+            webApiSounds[i].play(randomInteger(0, 2), randomInteger(0, 6));
+        }
+    }, timeToRefresh);
+}
+
+function stopUniqueLoop() {
+    for (let i = 0; i < webApiSounds.length; ++i) {
+        webApiSounds[i].volume = 0;
+    }
+}
---
+++ gif-control.js
@@ -0,0 +1,42 @@
+var playIcon = document.getElementById("play-icon");
+var playText = document.getElementById("play-text");
+var playGifAnimation = document.getElementById("play-gif-animation");
+
+function refreshClassnameAnimation() {
+    playGifAnimation.classList.remove("gif-webframe-anim");
+    void playGifAnimation.offsetWidth;
+    playGifAnimation.classList.add("gif-webframe-anim");
+}
+
+function changePlayOnPause() {
+    if (playIcon.id == "play-icon") {
+        playIcon.src = "assets/pause-water.svg";
+        playGifAnimation.src = "assets/animation.gif";
+        playText.src = "assets/pause-water-text.svg";
+
+        playIcon.id = "pause-icon";
+        playGifAnimation.id = "pause-gif-animation";
+        playText.id = "pause-text";
+        playText.className = "pause-water-text";
+    } else { 
+        playIcon = document.getElementById("pause-icon"); 
+        playText = document.getElementById("pause-text");
+
+        playIcon.src = "assets/Polygon-2.svg";
+        playGifAnimation.src = "assets/paused-gif.png";
+        playText.src = "assets/play-water.svg";
+
+        playIcon.id = "play-icon";
+        playGifAnimation.id = "play-gif-animation";
+        playText.id = "play-text";
+        playText.className = "play-water-text";
+
+        refreshClassnameAnimation();
+    }
+
+    if (playGifAnimation.id == "pause-gif-animation") {
+        generateUniqueLoop(3000);
+    } else {
+        stopUniqueLoop();
+    }
+}
